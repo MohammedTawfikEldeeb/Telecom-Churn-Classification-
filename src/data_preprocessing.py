5d2133de7c6aed9cb145791753ea10c9d5ad8a9f
Mohamed Tawfik <mohamed.tawfik.eldeeb@gmail.com>
2025-08-07 19:36:27 +0300

    add dvc

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -3,12 +3,12 @@ import numpy as np
 import os
 import yaml
 
-df_processed = pd.read_csv("dataset/telecom.csv")
+df_processed = pd.read_csv("datasets/telecom.csv")
 
 df_processed.drop(['area_code','phone_number'] , axis = 1 , inplace = True)
 df_processed['international_plan'] = df_processed['international_plan'].map({'no' : 0 , 'yes' : 1})
 df_processed['voice_mail_plan'] = df_processed['voice_mail_plan'].map({'no' : 0 , 'yes' : 1})
 
-data_path = os.path.join("data","processed")
+data_path = "datasets"
 
 df_processed.to_csv(os.path.join(data_path , "data_processed.csv"))
